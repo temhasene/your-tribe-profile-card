9ac55ddd7c710a77e9a9a006e305d062967a86af
temhasene <ahmet.kucuktepe@hva.nl>
2026-09-04 10:07:16 +0200

    summary
    ik had mijn code per ongeluk in de i-love-web gedeelte ingediend, maar realiseerde mij dat dat fout was en heb ik hem in de tribe-profile card gestuurd.

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,1 @@
-/* Stap 1: Selecteer de knop */
-let expandButton = document.querySelector('button')
 
-/* Stap 2: Wacht tot iemand op die knop klikt */
-expandButton.addEventListener('click', function() {
-
-  /* Stap 3: Verander een class zodra iemand op die knop klikt */
-  document.body.classList.toggle('is-expanded')
-})
